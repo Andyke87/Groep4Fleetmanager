9bd0620518545295faff1eb029d96adf4b83f3b2
Andyke87 <andy.lauwers@student.hogent.be>
2024-01-18 03:02:18 +0100

    x

--- FleetmanagerBackend/obj/Debug/net8.0/Back-end.AssemblyInfo.cs
+++ FleetmanagerBackend/obj/Debug/net8.0/Back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd19c9d3f69f6a474b1762a40574000a42743477")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e23bdccf054f55d99184e89f5a8f99d851e90ccc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
